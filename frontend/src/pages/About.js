5b1329047aab56b72143406109c8f6feba2996e8
Geetika-20 <geetikarathore20@gmail.com>
2022-06-28 22:40:41 +0530

    Mern Project

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>About</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Welcome, Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur voluptatem laudantium veritatis aspernatur voluptatum
+         corrupti itaque iste necessitatibus dicta incidunt? 
+         </p>
+         <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis nulla reprehenderit doloribus inventore sapiente quae, quas minima excepturi similique labore iusto voluptatum modi dicta provident odit nam voluptatibus hic quo accusamus at quaerat libero. Voluptate dolorum porro ea suscipit odio amet, repellendus optio illum quasi animi pariatur, aliquid sapiente voluptates!
+         </p>
+         <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nostrum necessitatibus distinctio! Velit id saepe rem cum impedit, voluptatum sit fugiat debitis! Perferendis aperiam sed vel minus dolorum dignissimos quam? Laborum aperiam quam assumenda, numquam tempore veniam reprehenderit earum facilis. Eum facere commodi voluptas voluptatem error qui provident nostrum cupiditate.</p>
+    </div>
+  )
+}
+
+export default About
